d6576f643e0c5c72b6afdb137e3fb2f54c1a054b
Janusz Twardak <ende.pld@gmail.com>
2022-03-30 10:24:11 +0200

    Improved Isolation-Forest-Extended.py and Visualize.py

--- Isolation-Forest-sklearn.py
+++ Isolation-Forest-sklearn.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 from sklearn.ensemble import IsolationForest
+
 import uproot
 from matplotlib import pyplot as plt
 import sys
@@ -10,23 +11,21 @@ sys.path.append(".")
 from Visualize import Visualize
 
 
-MAX_SAMPLES = 100
-
-ROOT_FILE_NAME = "331020_afp_newhits"
-PREPROCESSED_INPUT_PATH = "preprocessed_data/" + ROOT_FILE_NAME + ".pkl"
-ENTRY_LIMIT = 100
-
-
-# Load the two datasets
-df = pd.read_pickle(PREPROCESSED_INPUT_PATH)
-print(df.head())
-
-
 rng = np.random.RandomState(0)
 
 # Helper function to train and predict IF model for a feature
 def train_and_predict_if(df, feature):
-    clf = IsolationForest(max_samples=MAX_SAMPLES, random_state=rng)
+    """_summary_
+
+    :param df: _description_
+    :type df: _type_
+    :param feature: _description_
+    :type feature: _type_
+    :return: _description_
+    :rtype: _type_
+    """
+    clf = IsolationForest(random_state=rng)
+    print(df[[feature]])
     clf.fit(df[[feature]])
     pred = clf.predict(df[[feature]])
     scores = clf.decision_function(df[[feature]])
@@ -55,30 +54,6 @@ def print_all_outliers(df, feature):
 
 
 # Run through all features and save the outlier scores for each feature
-num_columns = [
-    i
-    for i in list(df.columns)
-    if i not in list(df.select_dtypes("object").columns) and i not in ["evN"]
-]
-
-result = pd.DataFrame()
-
-for feature in num_columns:
-    stats = train_and_predict_if(df, feature)
-    result = pd.concat([result, stats])
-
-# Gather top outliers for each feature
-outliers = {
-    team: grp.drop("feature", axis=1)
-    for team, grp in result.sort_values(by="score").groupby("feature")
-}
-
-# Print the top 10 outlier samples for a few selected features
-n_outliers = 10
-print_outliers(outliers, "hits_n", n_outliers)
-print_outliers(outliers, "hit_row_1", n_outliers)
-print_outliers(outliers, "hit_row_2", n_outliers)
-print_outliers(outliers, "hit_column_1", n_outliers)
 
 
 # event_number = 1344083997
@@ -86,8 +61,48 @@ print_outliers(outliers, "hit_column_1", n_outliers)
 # print(df.loc[df["evN"] == event_number])
 
 
+def main():
+    ROOT_FILE_NAME = "336505_afp_newhits"
+    PREPROCESSED_INPUT_PATH = "preprocessed_data/" + ROOT_FILE_NAME + ".pkl"
+
+    # Load the two datasets
+    df = pd.read_pickle(PREPROCESSED_INPUT_PATH)
+    print(df.head())
+
+    df = df.dropna()
+    num_columns = [
+        i
+        for i in list(df.columns)
+        if i not in list(df.select_dtypes("object").columns)
+        and i not in ["evN", "side"]
+    ]
+
+    result = pd.DataFrame()
+
+    for feature in num_columns:
+        stats = train_and_predict_if(df, feature)
+        result = pd.concat([result, stats])
+
+    # Gather top outliers for each feature
+    outliers = {
+        team: grp.drop("feature", axis=1)
+        for team, grp in result.sort_values(by="score").groupby("feature")
+    }
+
+    # Print the top 10 outlier samples for a few selected features
+    n_outliers = 10
+    print_outliers(outliers, "hits_n", n_outliers)
+    print_outliers(outliers, "hit_row_1", n_outliers)
+    print_outliers(outliers, "hit_row_2", n_outliers)
+    print_outliers(outliers, "hit_column_1", n_outliers)
+
+
 if __name__ == "__main__":
+    main()
     visualize = Visualize(
-        root_file_name="331020_afp_newhits", should_save=True, should_show=True
+        input_dataset="336505_afp_newhits", should_save=True, should_show=True
     )
-    visualize.draw_planes(1344083997)
+
+    i = 0
+
+    visualize.draw_planes(853927973)
--- Visualize.py
+++ Visualize.py
@@ -1,46 +1,77 @@
+from typing import Optional, Union
 import numpy as np
 from matplotlib import pyplot as plt
 import uproot
+import time
+import pandas as pd
+from pathlib import Path
 
 
 class Visualize:
-    def __init__(self, root_file_name, should_save, should_show):
+    def __init__(
+        self,
+        input_dataset: Union[pd.DataFrame, str],
+        should_save: bool,
+        should_show: bool,
+        scores: Union[pd.DataFrame, str],
+        anomaly_threshold: float,
+        root_name: str,  # TODO change to optional parameter in final version and add such handling
+    ) -> None:
+
         self.should_save = should_save
         self.should_show = should_show
-        self.root_file_name = root_file_name
+        self.dataset = self.load_dataset(input_dataset)
+        self.scores = self.load_scores(scores)
+        self.root_name = root_name
+        self.anomaly_threshold = anomaly_threshold
 
-        ENTRY_LIMIT = 100  # TODO DELETE AFTER TESTING
-        ROOT_INPUT_PATH = "input_root/" + self.root_file_name + ".root"
+    def load_dataset(self, input_dataset: Union[pd.DataFrame, str]) -> pd.DataFrame:
+
+        return (
+            input_dataset
+            if isinstance(input_dataset, pd.DataFrame)
+            else self.load_root_from_path(input_dataset)
+        )
+
+    @staticmethod
+    def load_scores(input_scores: Union[pd.DataFrame, str]) -> pd.DataFrame:
+
+        return (
+            input_scores
+            if isinstance(input_scores, pd.DataFrame)
+            else pd.read_pickle(input_scores)
+        )
+
+    @staticmethod
+    def load_root_from_path(root_name: str) -> pd.DataFrame:
+        ROOT_INPUT_PATH = "input_root/" + root_name + ".root"
 
         file = uproot.open(ROOT_INPUT_PATH)
         tree = file["TreeHits"]
-        raw_dataset = tree.arrays(
+        dataset = tree.arrays(
             ["evN", "hits", "hits_row", "hits_col", "hits_q"],
             library="pd",
-            entry_stop=ENTRY_LIMIT,  # TODO DELETE AFTER TESTING
         )
 
-        self.raw_dataset = raw_dataset
+        return dataset
 
-    def load_root(self):
-        ENTRY_LIMIT = 100  # TODO DELETE AFTER TESTING
-        ROOT_INPUT_PATH = "input_root/" + self.root_file_name + ".root"
+    def draw_histogram(self, bins: Optional[int] = 30) -> None:
+        plt.hist(self.scores, density=True, bins=bins)
+        plt.show()
 
-        file = uproot.open(ROOT_INPUT_PATH)
-        tree = file["TreeHits"]
-        raw_dataset = tree.arrays(
-            ["evN", "hits", "hits_row", "hits_col", "hits_q"],
-            library="pd",
-            entry_stop=ENTRY_LIMIT,  # TODO DELETE AFTER TESTING
+    def create_image_as_matrix(
+        self,
+        event: pd.DataFrame,
+    ) -> np.ndarray:
+
+        DETECTORS_NUMBER = 4
+        PLANES_PER_DETECTOR = 4
+        Y_PIXELS = 336
+        X_PIXELS = 80
+
+        image_matrix = np.zeros(
+            shape=(DETECTORS_NUMBER, PLANES_PER_DETECTOR, Y_PIXELS, X_PIXELS)
         )
-        return raw_dataset
-
-    def draw_planes(self, event_number):
-        event_matrix = np.zeros(shape=(4, 4, 336, 80))
-
-        event = self.raw_dataset.loc[self.raw_dataset["evN"] == event_number]
-        # event["a_hits_n"] = event.filter(regex="^hits\\[[01]", axis=1).sum(axis=1)
-        # event["c_hits_n"] = event.filter(regex="^hits\\[[23]", axis=1).sum(axis=1)
 
         regex_row = "^hits_row\\[X]\\[X"
         regex_col = "^hits_col\\[X]\\[X"
@@ -58,16 +89,39 @@ class Visualize:
                 hit_rows = hit_rows.transpose()
                 hit_columns = hit_columns.transpose()
 
-                hit_rows.rename(columns={4: "value"}, inplace=True)
-                hit_columns.rename(columns={4: "value"}, inplace=True)
+                hit_rows.set_axis(["value"], axis=1, inplace=True)
+                hit_columns.set_axis(["value"], axis=1, inplace=True)
 
                 for index, y in enumerate(hit_rows["value"]):
                     if y != -1:
                         x = hit_columns["value"].iloc[index]
+                        # TODO: charge_value instead of fixed 1 it should hold normalised value of charge
                         charge_value = 1
-                        event_matrix[detector, plane, y, x] = charge_value
+                        image_matrix[detector, plane, y, x] = charge_value
 
-        fig = plt.figure(dpi=300)
+        return image_matrix
+
+    def draw_planes(
+        self,
+        event_number: int,
+        should_show: Optional[bool] = None,
+        should_save: Optional[bool] = None,
+        save_path: Optional[str] = None,
+        detectors_side: Optional[str] = None,
+        image_dpi: Optional[int] = 300,
+        mark_anomalies: Optional[bool] = True,
+    ) -> None:
+
+        should_show = should_show or self.should_show
+        should_save = should_save or self.should_save
+
+        event = self.dataset.loc[self.dataset["evN"] == event_number]
+        # event["a_hits_n"] = event.filter(regex="^hits\\[[01]", axis=1).sum(axis=1)
+        # event["c_hits_n"] = event.filter(regex="^hits\\[[23]", axis=1).sum(axis=1)
+
+        image_matrix = self.create_image_as_matrix(event)
+
+        fig = plt.figure(dpi=image_dpi)
         rows = 2
         columns = 8
 
@@ -76,15 +130,33 @@ class Visualize:
             for plane in range(4):
                 fig.add_subplot(rows, columns, position)
                 position += 1
-                # TODO plane and detector should be swapped, probably saving error?
-                plt.imshow(event_matrix[plane][detector])
+                # TODO plane and detector should be swapped, probably some error?
+                plt.imshow(image_matrix[plane][detector])
                 plt.axis("off")
                 plt.tight_layout()
 
         if self.should_save:
-            name = "output/" + self.root_file_name + "_" + str(event_number) + ".png"
-            print(name)
-            plt.savefig(name)
+            save_path = save_path or ("/output/" + self.root_name)
+            save_path = save_path + "/" if save_path[-1] != "/" else save_path
+            Path(save_path).mkdir(parents=True, exist_ok=True)
+
+            if mark_anomalies:
+                is_anomaly = "anomaly" if is_anomaly else "normal"
+            else:
+                is_anomaly = ""
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+            save_path = (
+                save_path + str(event_number) + "_" + timestr + is_anomaly + ".png"
+            )
+            plt.savefig(save_path)
 
         if self.should_show:
             plt.show()
+
+        # TODO: draw_examples([...])
+        # def draw_examples(
+        #     dataset: pd.DataFrame, scores: pd.DataFrame, examples_number: Optional[int] = 10
+        # ):
+        #     scores.sort_values(by="score", ascending=True)
+
+        #     for image_counter in range(examples_number):

c896407048e5c2d15e6115c4e980d90dacf779b6
Janusz Twardak <ende.pld@gmail.com>
2022-04-13 15:05:06 +0200

    Added bugged Isolation Forest

--- Isolation-Forest-Extended.py
+++ Isolation-Forest-Extended.py
@@ -5,88 +5,70 @@ import numpy as np
 from sklearn.ensemble import IsolationForest
 from sympy import true
 import uproot
+from typing import List, Optional
 from matplotlib import pyplot as plt
 import sys
 from isotree import IsolationForest
-
-
-sys.path.append(".")
+import pickle
 from Visualize import Visualize
 
-ROOT_FILE_NAME = "331020_afp_newhits"
-PREPROCESSED_INPUT_PATH = "preprocessed_data/" + ROOT_FILE_NAME + ".pkl"
 
-
-def load_preprocessed_data(PREPROCESSED_INPUT_PATH):
-    df = pd.read_pickle(PREPROCESSED_INPUT_PATH)
-    print(df.head())
+def load_preprocessed_data(
+    preprocessed_input_path: str, drop_evN: Optional[bool] = True
+) -> pd.DataFrame:
+    df = pd.read_pickle(preprocessed_input_path)
+    df.set_index(df["evN"], inplace=True)
+    if drop_evN:
+        df.drop("evN", axis=1, inplace=True)
+    df.drop("side", axis=1, inplace=True)
+    print(df)
     return df
 
 
-def plot_space(Z, space_index, X):
-    df = pd.DataFrame({"z": Z}, index=space_index)
-    df = df.unstack()
-    df = df[df.columns.values[::-1]]
-    plt.imshow(df, extent=[-3, 3, -3, 3], cmap="hot_r")
-    plt.scatter(x=X["x"], y=X["y"], alpha=0.15, c="navy")
+def train_model(preprocessed_df_names: List[str]) -> IsolationForest:
+
+    model = IsolationForest(ndim=1, ntrees=100)
+
+    for filename in preprocessed_df_names:
+        preprocessed_input_path = "preprocessed_data/" + filename + ".pkl"
+
+        dataset = load_preprocessed_data(preprocessed_input_path)
+
+        model = model.fit(dataset)
+    return model
 
 
-# def planes_preview(visualizer, events_number_to_show=10):
-#     normal_counter = 0
-#     anomaly_counter = 0
+def main() -> None:
 
-#     visualizer.draw_planes(1344530188, path="output/331020_afp_newhits/")
+    dataset_name = "331020_afp_newhits"
+    preprocessed_input_path = "preprocessed_data/" + dataset_name + ".pkl"
+    dataset_test = load_preprocessed_data(preprocessed_input_path)
 
-#     for i, row in loaded_data.iterrows():
-#         if row["score"] > 0.7 and anomaly_counter < 10:
-#             visualizer.draw_planes(
-#                 1344989546, path="output/331020_afp_newhits/anomaly/"
-#             )
-#             anomaly_counter = anomaly_counter + 1
-#         elif normal_counter < 10:
-#             visualizer.draw_planes(1344989546, path="output/331020_afp_newhits/normal/")
-#             normal_counter = normal_counter + 1
+    preprocessed_df_names = ["331020_afp_newhits", "336505_afp_newhits"]
 
+    model = train_model(preprocessed_df_names)
 
-def main():
+    scores = pd.DataFrame(model.predict(dataset_test))
 
-    loaded_data = load_preprocessed_data(PREPROCESSED_INPUT_PATH)
-    loaded_data.set_index(loaded_data["evN"], inplace=True)
-    dataset = pd.concat(
-        [loaded_data["hits_n"], loaded_data["evN"]], axis=1, ignore_index=False
-    )
+    dataset_test = load_preprocessed_data(preprocessed_input_path, drop_evN=False)
+    scores.set_index(dataset_test["evN"], inplace=True)
 
-    space = (
-        np.array(
-            np.meshgrid(
-                loaded_data["hits_n"],
-                loaded_data["hit_row_1"],
-            )
-        )
-        .reshape((2, -1))
-        .T
-    )
-
-    space_index = pd.MultiIndex.from_arrays([space[:, 0], space[:, 1]])
-
-    model = IsolationForest(ndim=1, ntrees=100).fit(dataset)
-    scores = pd.DataFrame(model.predict(dataset))
-    scores.set_index(loaded_data["evN"], inplace=True)
-    threshold = 0.6
-
-    # print(loaded_data.sort_values(by="score", ascending=True))
+    print(scores.head())
+    anomaly_threshold = 0.6
 
     visualizer = Visualize(
-        input_dataset=ROOT_FILE_NAME,
+        input_dataset=dataset_name,
         scores=scores,
-        anomaly_threshold=threshold,
+        anomaly_threshold=anomaly_threshold,
         should_save=False,
         should_show=True,
-        root_name=ROOT_FILE_NAME,
+        root_name=dataset_name,
     )
 
-    # visualizer.draw_planes(event_number=1344053190, side="c")
     visualizer.draw_examples(examples_number=10)
+
+    # pickle.dump(model, open("output/", ROOT_FILE_NAME, "/model.pck"))
+    # pickle.dump(scores, open("output/", ROOT_FILE_NAME, "/scores.pck"))
 
 
 if __name__ == "__main__":
